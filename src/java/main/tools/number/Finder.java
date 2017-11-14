546308334bd756f4fde084e180ab0e0c12429ac7
chris <chris@DESKTOP-8NRPQMF>
2017-11-14 15:10:46 -0500

    Applying enhancement, Issue #3.

--- src/java/main/tools/number/Finder.java
+++ src/java/main/tools/number/Finder.java
@@ -1,23 +1,28 @@
 package tools.number;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 public class Finder {
 		
 	/**
-	 * 
-	 * @param arr Array of numbers
-	 * @param k Desired number to find in array.
+	 * @param numberArray Array of numbers
+	 * @param number Desired number to find in array.
 	 * @return "YES" if number is found in the array. "NO" if not found.
 	 * @throws RuntimeException
 	 */
-	public static boolean specificNumber(final int arr[], final int k){
+	public static boolean specificNumber(final int numberArray[], final int number){
 	    
 		try{
 			
-	        for(int i = 0; i < arr.length; i++){
+	        for(int i = 0; i < numberArray.length; i++){
 	        	
-	        	if(arr[i] == k){
+	        	if(numberArray[i] == number){
 	        		return true;
 	        	}
 	        	
@@ -34,7 +39,6 @@ public class Finder {
 	}
 	
 	/**
-	 * 
 	 * @param a The value of which the search should start at.
 	 * @param b The value of which the search should end at.
 	 * @return 
@@ -86,5 +90,221 @@ public class Finder {
 	    	
     }
     
+    /**
+     * @param numberArray Array of numbers
+     * @return MEAN (average) of all numbers in array.
+     */
+    public static int getMean(final int[] numberArray){
+    	
+    	int average = 0;
+    	
+    	for(int i : numberArray){
+    		
+    		average += i;
+    		
+    	}
+    	
+    	return average / numberArray.length;
+    	
+    }
+    
+    /**
+     * @param numberArray Array of numbers
+     * @return MEDIAN (middle most number) of all numbers in array.
+     */
+    public static int getMedian(final int[] numberArray){
+    	
+    	Integer sortedNumberArray[] = new Integer[numberArray.length];
+    	int median = 0;
+    	
+    	for(int i = 0; i < numberArray.length; i++){
+    		
+    		sortedNumberArray[i] = new Integer(numberArray[i]);
+    		
+    	}
+    	
+    	Arrays.sort(sortedNumberArray);
+    	median = sortedNumberArray.length / 2;
+    	
+    	return sortedNumberArray[median];
+    	
+    }
+    
+    /**
+     * @param numberArray Array of numbers
+     * @return Numeric distance between the lesser and greater values from within the array.
+     */
+    public static int getRange(final int[] numberArray){
+    	
+    	Integer sortedValue[] = new Integer[numberArray.length];
+    	
+    	int first = 0;
+    	int last = 0;
+    	
+    	for(int i = 0; i < numberArray.length; i++){
+    		
+    		sortedValue[i] = new Integer(numberArray[i]);
+    		
+    	}
+    	
+    	Arrays.sort(sortedValue);
+    	first = sortedValue[0];
+    	last = sortedValue[sortedValue.length-1];
+    	
+    	return last - first;
+    	
+    }
+    
+    /**
+     * @param numberArray Array of numbers
+     * @return MODE (greatest repeating numbers) from all numbers in array.
+     * @throws RuntimeException
+     */
+    public static List<Integer> getMode(final int[] numberArray){
+    	
+    	Map<Integer,Integer>duplicateCountMap = null;
+    	List<Integer>countList = new ArrayList<>(numberArray.length);
+    	List<Integer>modeList = new ArrayList<>();
+    	List<Integer>duplicateList = null;
+    	Integer maxCount = 0;
+    	
+    	try{
+    		
+    		// Get duplicates and their number of recurrences.
+    		duplicateList = getDuplicateNumbers(numberArray);
+    		duplicateCountMap = getDuplicateCountMap(numberArray);
+    		
+    		if( duplicateList.isEmpty() || duplicateCountMap.isEmpty() ){
+    			
+    			return modeList;
+    			
+    		} else {
+    			
+    			// Sort the recurrences count, largest first
+        		countList = Arrays.asList( duplicateCountMap.values().toArray( new Integer[duplicateCountMap.size()] ) );
+            	Collections.sort(countList);
+            	Collections.reverse(countList);
+            	
+            	// Get largest count, including duplicates if present.
+            	for(int i = 0; i < countList.size(); i++){
+            		
+            		if( countList.get(0).equals( countList.get(i) ) ){
+            			
+            			maxCount++;
+            			
+            		}
+            		
+            	}
+            	
+            	// Get MODES
+        		int i = 0;
+	        	for(Integer duplicate : duplicateList){
+	        		
+	        		
+        			if( !duplicateCountMap.isEmpty() 
+        					&& duplicateCountMap.get(duplicate).equals( countList.get(i) ) 
+        					&& i < maxCount ){
+        				
+        				modeList.add(duplicate);
+        				duplicateCountMap.remove(duplicate);
+        				
+        			}
+	        			
+	        	}
+        	
+            	return modeList;
+    			
+    		}
+    		        	
+    	} catch(RuntimeException e){
+    		
+    		throw e;
+    		
+    	}
+    	
+    }
 
+    /**
+     * @see tools.number.Finder#getDuplicateCountMap(int[])
+     * @see tools.number.Finder#getMode(int[])
+     * @param numberArray Array of numbers
+     * @return List of unique duplicate numbers.
+     */
+    private static List<Integer> getDuplicateNumbers(final int[] numberArray){
+    	
+    	HashSet<Integer>originals = new HashSet<>();
+    	HashSet<Integer>duplicateSet = new HashSet<>();
+
+    	// Find duplicates
+    	for(int i = 0; i < numberArray.length; i++){
+    		
+    		if(!originals.add(numberArray[i])){
+    			
+    			duplicateSet.add(numberArray[i]);
+    			
+    		}
+    		
+    	}
+    	
+    	// Reverse sort duplicates if any or return null
+    	if(duplicateSet.isEmpty()){
+    		
+    		return new ArrayList<Integer>();
+    		
+    	}else{
+
+    		return Arrays.asList( duplicateSet.toArray( new Integer[duplicateSet.size()] ) );
+    	}
+    	
+    }
+    
+    /**
+     * @see tools.number.Finder#getMode(int[])
+     * @param numberArray Array of numbers
+     * @return Map of duplicates and counted recurrences within a given array.
+     * @throws IllegalStateException
+     */
+    private static Map<Integer, Integer> getDuplicateCountMap(final int[] numberArray){
+    	
+    	List<Integer>duplicateList = getDuplicateNumbers(numberArray);
+    	Map<Integer, Integer>duplicateMap = new HashMap<>();
+    	
+    	if( duplicateList == null ){
+    		
+    		throw new IllegalStateException("Null value disallowed!");
+    		
+    	}
+    	if( duplicateList.isEmpty() ){
+    		
+    		return duplicateMap;
+    		
+    	}
+    	else{
+        	
+    		for(Integer duplicate : duplicateList){
+    			
+    			duplicateMap.put(duplicate, 0);
+    			
+    		}
+    		 	
+        	for(Integer duplicate : duplicateList){
+        		
+        		for(int element : numberArray){
+        			
+        			if(duplicate.equals(element)){
+        				
+        				duplicateMap.replace(duplicate, duplicateMap.get(duplicate), duplicateMap.get(duplicate) + 1);
+        				
+        			}
+        			
+        		}
+        		
+        	}
+        	
+        	return duplicateMap;
+        	
+    	}
+
+    }
+    
 }
--- src/java/main/tools/number/FinderTest.java
+++ src/java/main/tools/number/FinderTest.java
@@ -3,6 +3,8 @@ package tools.number;
 import static tools.number.Finder.oddNumbers;
 import static tools.number.Finder.specificNumber;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,13 +12,15 @@ public class FinderTest {
 	
 	// Test specificNumber(int, int)
 	@Test
-	public void testPositiveNumber() {		
+	public void testSpecificNumberPositiveNumber() {	
+		
 		int i[] = {1,3,5};
 		Assert.assertTrue( specificNumber(i, 3) );
+		
 	}
 	
 	@Test
-	public void testPositiveNumberNotExist() {	
+	public void testSpecificNumberPositiveNumberNotExist() {	
 		
 		int i[] = {1,3,5};
 		Assert.assertFalse( specificNumber(i, 2) );
@@ -24,7 +28,7 @@ public class FinderTest {
 	}
 	
 	@Test
-	public void testNegativeNumber(){
+	public void testSpecificNumberNegativeNumber(){
 		
 		int i[] = {-1,-3,-5};
 		Assert.assertTrue( specificNumber(i, -3) );
@@ -34,59 +38,212 @@ public class FinderTest {
 	
 	// Test oddNumbers(int, int)
 	@Test
-	public void testLesserToGreater() {
+	public void testOddNumbersLesserToGreater() {
 					
 		int i[] = {1,3,5};
-		Assert.assertArrayEquals(i, oddNumbers(1,5));
+		Assert.assertArrayEquals( i, oddNumbers(1,5) );
 					
 	}
 	
 	@Test
-	public void testGreaterToLesser() {
+	public void testOddNumbersGreaterToLesser() {
 		
 		int i[] = {5,3,1};
-		Assert.assertArrayEquals(i, oddNumbers(5,1));
+		Assert.assertArrayEquals( i, oddNumbers(5,1) );
 		
 	}
 	
 	@Test
-	public void testNegativeLesserToGreater(){
+	public void testOddNumbersNegativeLesserToGreater(){
 		
 		int i[] = {-5,-3,-1};
-		Assert.assertArrayEquals(i, oddNumbers(-5,-1));
+		Assert.assertArrayEquals( i, oddNumbers(-5,-1) );
 		
 	}
 	
 	@Test
-	public void testNegativeGreaterToLesser(){
+	public void testOddNumbersNegativeGreaterToLesser(){
 		
 		int i[] = {-1,-3,-5};
-		Assert.assertArrayEquals(i, oddNumbers(-1,-5));
+		Assert.assertArrayEquals( i, oddNumbers(-1,-5) );
 		
 	}
 	
 	@Test
-	public void testNegativeToPositive(){
+	public void testOddNumbersNegativeToPositive(){
 		
 		int i[] = {-5,-3,-1,1,3,5};
-		Assert.assertArrayEquals(i, oddNumbers(-5,5));
+		Assert.assertArrayEquals( i, oddNumbers(-5,5) );
 		
 	}
 	
 	@Test
-	public void testDuplicatePositiveValue(){
+	public void testOddNumbersDuplicatePositiveValue(){
 		
 		int i[] = {5};
-		Assert.assertArrayEquals(i, oddNumbers(5,5));
+		Assert.assertArrayEquals( i, oddNumbers(5,5) );
 		
 	}
 	
 	@Test
-	public void testDuplicateNegativeValue(){
+	public void testOddNumbersDuplicateNegativeValue(){
 		
 		int i[] = {-5};
-		Assert.assertArrayEquals(i, oddNumbers(-5,-5));
+		Assert.assertArrayEquals( i, oddNumbers(-5,-5) );
 		
 	}	
+	
+	
+	// Test getMean(int[])
+	@Test
+	public void testGetMeanPostiveNumbers(){
+		
+		int i[] = {1, 2, 3};
+		Assert.assertEquals( 2, Finder.getMean(i) );
+		
+	}
+	
+	@Test
+	public void testGetMeanNegativeNumbers(){
+		
+		int i[] = {-1, -2, -3};
+		Assert.assertEquals( -2, Finder.getMean(i) );
+		
+	}
+	
+	@Test
+	public void testGetMeanNegativeAndPostiveNumbers(){
+		
+		int i[] = {-1, -2, 6};
+		Assert.assertEquals( 1, Finder.getMean(i) );
+		
+	}
+	
+	@Test
+	public void testGetMeanIfResultZero(){
+		
+		int i[] = {-1, -2, 3};
+		Assert.assertEquals( 0, Finder.getMean(i) );
+		
+	}
+	
+	@Test
+	public void testGetMeanAllZeros(){
+		
+		int i[] = {0, 0, 0};
+		Assert.assertEquals( 0, Finder.getMean(i) );
+		
+	}
+	
+	
+	// Test getMedian(int[])
+	@Test
+	public void testGetMedianPositiveNumbers(){
+		
+		int i[] = {1, 2, 3};
+		Assert.assertEquals( 2, Finder.getMedian(i) );
+		
+	}
+	
+	@Test
+	public void testGetMedianNegativeNumbers(){
+		
+		int i[] = {-1, -2, -3};
+		Assert.assertEquals( -2, Finder.getMedian(i) );
+		
+	}
+	
+	@Test
+	public void testGetMedianPositiveAndNegativeNumbers(){
+		
+		int i[] = {-2, 0, 2};
+		Assert.assertEquals( 0, Finder.getMedian(i) );
+		
+	}
+	
+	@Test
+	public void testGetMedianAllZeroes(){
+		
+		int i[] = {0, 0, 0};
+		Assert.assertEquals( 0, Finder.getMedian(i) );
+		
+	}
+	
+	
+	// Test getRange(int[])
+	@Test
+	public void testGetRangePostiveNumbers(){
+		
+		int i[] = {1, 2, 3};
+		Assert.assertEquals( 2, Finder.getRange(i) );
+		
+	}
+	
+	@Test
+	public void testGetRangeNegativeNumbers(){
+		
+		int i[] = {-1, -2, -3};
+		Assert.assertEquals( 2, Finder.getRange(i) );
+		
+	}
+	
+	@Test
+	public void testGetRangePostiveAndNegativeNumbers(){
+		
+		int i[] = {-2, 0, 2};
+		Assert.assertEquals( 4, Finder.getRange(i) );
+		
+	}
+	
+	@Test
+	public void testGetRangeAllZeroes(){
+		
+		int i[] = {0, 0, 0};
+		Assert.assertEquals( 0, Finder.getRange(i) );
+		
+	}
+	
+	// Test getMode(int[])
+	@Test
+	public void testGetModePositiveNumbers(){
+		
+		int i[] = {1, 2, 3, 3, 4};
+		Assert.assertEquals( 3, Finder.getMode(i).get(0).intValue());		
+		
+	}
+	
+	@Test
+	public void testGetModeNegativeNumbers(){
+		
+		int i[] = {-1, -2, -3, -3, -4};
+		Assert.assertEquals( -3, Finder.getMode(i).get(0).intValue() );
+		
+	}
+	
+	@Test
+	public void testGetModePositiveAndNegativeNumbers(){
+		
+		int i[] = {1, 2, 3, -3, -3, -4};
+		Assert.assertEquals( -3, Finder.getMode(i).get(0).intValue() );
+		
+	}
+	
+	@Test
+	public void testGetModeMultipleModes(){
+		
+		int i[] = {1, 1, 1, -2, -3, -3, -3, -4};
+		List<Integer>mode = Finder.getMode(i);
+		Assert.assertEquals( 1, mode.get(0).intValue() );
+		Assert.assertEquals( -3, mode.get(1).intValue() );
+		
+	}
+	
+	@Test
+	public void testGetModeNoMode(){
+		
+		int i[] = {1, -2, 3, -4};
+		Assert.assertTrue( Finder.getMode(i).isEmpty() );
+		
+	}
 	
 }
